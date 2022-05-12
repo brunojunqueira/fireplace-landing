48c0f8a2d8dbb6a9f52bc9bee8aee4ed65d7dc49
Bruno Junqueira <bruno_cenu@hotmail.com>
2022-05-12 10:37:06 -0300

    link

--- components/Resources/index.js
+++ components/Resources/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import Link from 'next/dist/client/link'
 
 export default function Resources({title, sources = null}){
     return(
@@ -7,7 +6,7 @@ export default function Resources({title, sources = null}){
             <title>{title}</title>
             {sources?.map((source, index) =>{
                 return(
-                    <Link key={index} href={source.href} rel={source.rel}/>
+                    <link key={index} href={source.href} rel={source.rel}/>
                 )
             })}
         </Head>
