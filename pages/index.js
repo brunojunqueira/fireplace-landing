897ebfc2f72c551c949fd6313aeec84cd419fba7
Bruno Junqueira <bruno_cenu@hotmail.com>
2022-05-12 10:49:30 -0300

    locales

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import style from '../styles/home.module.css'
 
 export default function Home() {
 
-  let {t} = useTranslation(['global', 'home']);
+  const {t} = useTranslation();
 
   return (
     <div className={style.content}>
@@ -22,9 +22,9 @@ export default function Home() {
       />
       <main className={style.main}>
         <LogoEffect size={130} text={'À Lareira'}/>
-        <Link href='./login'><button className={style.loginbtn}>{t('login')}</button></Link>
-        <Link href='./register'><button className={style.registerbtn}>{t('register')}</button></Link>
-        <Link href='./feed'><button className={style.notnowbtn}>{t('notnow')}</button></Link>
+        <Link href='./login'><button className={style.loginbtn}>{t('global:login')}</button></Link>
+        <Link href='./register'><button className={style.registerbtn}>{t('global:register')}</button></Link>
+        <Link href='./feed'><button className={style.notnowbtn}>{t('global:notnow')}</button></Link>
       </main>
       <div className={style.arrowcontainer}>
         <span onClick={() => scrollTo({top: window.screenY, behavior:'smooth'})} className="material-icons">keyboard_arrow_down</span>
@@ -33,14 +33,14 @@ export default function Home() {
       <div className={style.infoContainer}>
         <div className={style.info}>
           <Image className={style.infoimg} src="https://unsplash.com/photos/S_VbdMTsdiA/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjM1MzAyNTI0&force=true" alt='image'/>
-          <span className={style.infotxt}>{t('t1', {ns: 'home'})}</span>
+          <span className={style.infotxt}>{t('home:t1')}</span>
         </div>
         <div className={style.info}>
-          <span className={style.infoimg}>{t('t2', {ns: 'home'})}</span>
+          <span className={style.infoimg}>{t('home:t2')}</span>
           <Image className={style.infotxt} src="https://unsplash.com/photos/XqXJJhK-c08/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjM1Mjc1NDg3&force=true" alt='image'/>
         </div>
         <div className={style.info}>
-          <span className={style.infotxt}>{t('l', {ns: 'home'})}</span>
+          <span className={style.infotxt}>{t('home:l')}</span>
         </div>
       </div>
       <Footer className={style.footer}/>
