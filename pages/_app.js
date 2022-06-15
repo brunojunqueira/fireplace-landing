f8a5d96aa86aecce8c3957baa940ac5ca9f2d1bf
brunojunqueira <bruno_cenu@hotmail.com>
2022-06-14 22:09:49 -0300

    Adding unfinished Login Page, create Main Component to organize the scheme better

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   <>
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
       <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet"/>
     </Head>
     <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,60 @@
 import useTranslation from 'next-translate/useTranslation'
-import Footer from '../components/Footer'
 import Link from 'next/dist/client/link'
 import Logo from '../components/Logo'
-import Resources from '../components/Resources'
+import Main from '../components/Main';
 
 import style from '../styles/home.module.scss'
 
 export default function Home() {
 
-  const { t } = useTranslation('home');
+    const { t } = useTranslation('home');
 
-  return (
-    <div className={style.content}>
-      <Resources 
-        title='Fireplace'
-        sources={[
-          {
-            href:'https://fonts.googleapis.com/icon?family=Material+Icons', 
-            rel:'stylesheet'
-          },
-          {
-            href:'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
-            rel:'stylesheet'
-          }
-        ]}
-      />
-      <main className={style.main}>
-        <Logo
-          style={{
-            height:'150px', 
-            width:'500px', 
-            paddingBottom:'75px'
-          }}
-          color='var(--alt-red)'
-        />
-        <Link href='./login'>
-          <button 
-            className={style.loginbtn}>
-              <span>
-                {t('login')}
-              </span>
-              <span className="material-symbols-outlined">
-                login
-              </span>
-          </button>
-        </Link>
-        <Link href='./register'>
-          <button 
-            className={style.registerbtn}>
-              {t('register')}
-          </button>
-        </Link>
-        <Link href='./feed'>
-          <button className={style.notnowbtn}>{t('notnow')}</button>
-        </Link>
-      </main>
-      <Footer className={style.footer}/>
-
-    </div>
-  )
+    return (
+        <Main 
+            title='Fireplace'
+            sources={[
+            {
+                href:'https://fonts.googleapis.com/icon?family=Material+Icons', 
+                rel:'stylesheet'
+            },
+            {
+                href:'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+                rel:'stylesheet'
+            }
+            ]}
+        >
+            <div className={style.content}>
+                <main className={style.main}>
+                    <Logo
+                    style={{
+                        height:'150px', 
+                        width:'500px', 
+                        paddingBottom:'75px'
+                    }}
+                    color='var(--alt-red)'
+                    />
+                    <Link href='./login'>
+                    <button 
+                        className={style.loginbtn}>
+                        <span>
+                            {t('login')}
+                        </span>
+                        <span className="material-symbols-outlined">
+                            login
+                        </span>
+                    </button>
+                    </Link>
+                    <Link href='./register'>
+                    <button 
+                        className={style.registerbtn}>
+                        {t('register')}
+                    </button>
+                    </Link>
+                    <Link href='./feed'>
+                    <button className={style.notnowbtn}>{t('notnow')}</button>
+                    </Link>
+                </main>
+            </div>
+        </Main>
+    )
 }
